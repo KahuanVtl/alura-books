823f3016091c73ae331678e2a0349a0fd886231a
Kahuan <kahuanvitelli@gmail.com>
2023-07-03 19:04:18 -0300

    Aprendendo React

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './Imagens/logo.svg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <div className='logo'>
+          <img src={logo} alt='Logo da Alura Books'></img>
+          <p><strong>Alura</strong> Books</p>
+        </div>
       </header>
     </div>
   );
